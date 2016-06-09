f0fe8987c9207d9b7352742c2222ac4bd5c73977
Necrostic <damiong28@gmail.com>
2016-06-09 18:34:56 +0000

    Add set_post before_action in Posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
         
   def index
     @posts = Post.all
@@ -14,15 +15,12 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find(params[:id])
   end
   
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:success] = "Post edited"
       redirect_to @post
@@ -32,7 +30,6 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -41,5 +38,9 @@ class PostsController < ApplicationController
   
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+  
+  def set_post
+    @post=Post.find(params[:id])
   end
 end
