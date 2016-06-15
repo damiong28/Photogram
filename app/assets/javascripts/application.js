709956b5b07103a2599bc675c4edf19a132c135a
Necrostic <damiong28@gmail.com>
2016-06-15 00:57:36 +0000

    Add comment styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,8 +18,10 @@ class CommentsController < ApplicationController
     @comment.user_id =  current_user.id
     
     if @comment.save
-      flash[:success] = "Comment saved"
-      redirect_to :back 
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js 
+      end
     else
       flash[:alert] = "Failed to create comment, please check your submission 
                             and try again"
